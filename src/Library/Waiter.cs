0ae9032f5bda9d22e5a3702371398d5960352c7a
Valentina Hernández Rovere <valentinahernandezrovere@MacBook-Air-de-Valentina.local>
2026-08-19 10:33:52 -0300

    Comentarios en Waiter

--- src/Library/Waiter.cs
+++ src/Library/Waiter.cs
@@ -11,27 +11,50 @@ namespace Ucu.Poo.Restaurant
     /// </summary>
     public class Waiter
     {
+        /// <summary>
+        /// Nombre del mozo.
+        /// </summary>
         public String name;
+        /// <summary>
+        /// Nombre del mozo.
+        /// </summary>
         public String Name
         {
             get{ return name; } set {name=value;}
         }
         private ArrayList assignedTables = new ArrayList();
+        /// <summary>
+        /// Mesas asignadas al mozo.
+        /// </summary>
         public ArrayList AssignedTables
         {
             get {return assignedTables;} set {assignedTables =value;}
         }
+        /// <summary>
+        /// Inicializa un mozo con su nombre.
+        /// </summary>
+        /// <param name="name">Nombre del mozo.</param>
         public Waiter(String name)
         {
             this.Name=name;
         }
+
+        /// <summary>
+        /// Asigna una mesa al mozo.
+        /// </summary>
+        /// <param name="table">Mesa que se asignará.</param>
         public void AssignTable(Table table)
         {
         assignedTables.Add(table);
         }
+        /// <summary>
+        /// Toma el pedido de un plato para una mesa.
+        /// </summary>
+        /// <param name="table">Mesa cuyo pedido se actualizará.</param>
+        /// <param name="dish">Plato que se agregará al pedido.</param>
         public void TakeOrder(Table table, Dish dish)
         {
-           table.AddToOrder(dish);
+           table.Order.Add(dish);
         }
 
     }
