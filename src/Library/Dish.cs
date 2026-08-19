7f065b80e481f52e8a66696d00093357bdd79a4d
Valentina Hernández Rovere <valentinahernandezrovere@MacBook-Air-de-Valentina.local>
2026-08-19 09:10:51 -0300

    Dishes subido

--- src/Library/Dish.cs
+++ src/Library/Dish.cs
@@ -4,12 +4,43 @@
 // </copyright>
 //------------------------------------------------------------------------------
 
+using System.Text.RegularExpressions;
+
 namespace Ucu.Poo.Restaurant
 {
-    /// <summary>
-    /// Representa un platillo individual en el menú del restaurante.
+     /// <summary>
+    /// Representa un plato en el restaurante.
     /// </summary>
     public class Dish
     {
+        private string name;
+        public string Name
+        {
+            get {return name;} 
+            set {
+                if (!string.IsNullOrEmpty(value))
+                {
+                name=value;
+                }
+            }
+        }
+        private string price;
+        public string Price
+        {
+            get{return price;}
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    price = value;
+                }
+            }
+        }
+        private bool isvegetarian;
+        public bool IsVegetarian
+        {
+            get {return isvegetarian;}
+            set {isvegetarian=value}
+        }
     }
 }
